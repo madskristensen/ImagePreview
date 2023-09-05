0da959e4136c5a3a6e775a2dbae25b34e31e8915
Mads Kristensen <madskristensen@users.noreply.github.com>
2023-09-05 11:40:16 -0700

    Added ability to open image from tooltip Fixes #11 [release]

--- src/QuickInfo/PreviewControl.xaml.cs
+++ src/QuickInfo/PreviewControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using Microsoft.VisualStudio.PlatformUI;
 
@@ -13,7 +14,7 @@ namespace ImagePreview.QuickInfo
         public PreviewControl()
         {
             InitializeComponent();
-            
+
             lblSize.SetResourceReference(TextBlock.ForegroundProperty, EnvironmentColors.ComboBoxFocusedTextBrushKey);
         }
 
@@ -30,21 +31,36 @@ namespace ImagePreview.QuickInfo
                 return false;
             }
 
-            if (result.Format == ImageFormat.GIF && Uri.TryCreate(url, UriKind.Absolute, out Uri absoluteUri))
+            bool isAbsoluteUri = Uri.TryCreate(url, UriKind.Absolute, out Uri absoluteUri);
+
+            if (result.Format == ImageFormat.GIF && isAbsoluteUri)
             {
                 mediaPreview.Source = absoluteUri;
                 mediaPreview.Visibility = Visibility.Visible;
+                SetupClickHandler(isAbsoluteUri, absoluteUri, mediaPreview);
+
             }
             else
             {
                 imgPreview.Source = bitmap;
                 imgPreview.Visibility = Visibility.Visible;
+                SetupClickHandler(isAbsoluteUri, absoluteUri, imgPreview);
             }
 
             lblSize.Content = $"{Math.Round(bitmap.Width)}x{Math.Round(bitmap.Height)} ({result.FileSize.ToFileSize(2)})";
             panel.Visibility = Visibility.Visible;
 
             return true;
+        }
+
+        private void SetupClickHandler(bool isAbsoluteUri, Uri absoluteUri, FrameworkElement element)
+        {
+            if (isAbsoluteUri)
+            {
+                element.Cursor = Cursors.Hand;
+                element.ToolTip = "Click to open image in default application";
+                element.MouseUp += (s, e) => { System.Diagnostics.Process.Start(absoluteUri.OriginalString); };
+            }
         }
     }
 }
